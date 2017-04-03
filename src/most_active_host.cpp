060e5d2e51ff23e11f685084ef1fb5c4d83b94a2
Wenjun Ouyang <ouyangw@sas.upenn.edu>
2017-04-03 02:09:31 -0400

    Add MostActiveHost class for maintaining a list of N most active host.

---
+++ src/most_active_host.cpp
@@ -0,0 +1,59 @@
+#include "most_active_host.hpp"
+#include <utility>
+#include <cassert>
+#include <sstream>
+
+////////////////////////////////////////////////////////////////////////////////
+
+MostActiveHost::MostActiveHost(size_t N): N_(N)
+{
+  firstN_.reserve(N);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void MostActiveHost::processLine(const LogEntry &line)
+{
+  infoMap_[line.host].count += 1;
+  Info &info(infoMap_[line.host]);
+  if (info.isFirstN && info.idx > 0) {
+    // find the new position in the vector.
+    size_t newIdx(info.idx);
+    for (; newIdx > 0; --newIdx)
+      if (infoMap_[firstN_[newIdx - 1]].count >= info.count) break;
+    assert(infoMap_[firstN_[newIdx]].idx == newIdx);
+    const size_t oldIdx(info.idx);
+    infoMap_[firstN_[newIdx]].idx = oldIdx;
+    info.idx = newIdx;
+    std::swap(firstN_[newIdx], firstN_[oldIdx]);
+  } else if (!info.isFirstN) {
+    if (firstN_.size() < N_) {
+      // the vector is not full, just push
+      firstN_.push_back(line.host);
+      info.isFirstN = true;
+      info.idx = firstN_.size() - 1;
+    } else if (info.count > infoMap_[firstN_.back()].count) {
+      // new count is large than the smallest count in the vector
+      // replace one entry from the vector
+      size_t newIdx(N_ - 1);
+      for (; newIdx > 0; --newIdx)
+        if (infoMap_[firstN_[newIdx - 1]].count >= info.count) break;
+      infoMap_[firstN_[newIdx]].isFirstN = false;
+      info.isFirstN = true;
+      info.idx = newIdx;
+      firstN_[newIdx] = line.host;
+    }
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+std::string MostActiveHost::getStat() noexcept
+{
+  std::stringstream ss;
+  for (const std::string &s: firstN_)
+    ss << s << ','
+       << infoMap_[s].count << '\n';
+  return ss.str();
+}
+
---
+++ src/most_active_host.hpp
@@ -0,0 +1,35 @@
+////////////////////////////////////////////////////////////////////////////////
+// Maintain the information of N most active host
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef MOST_ACTIVE_HOST_HPP
+#define MOST_ACTIVE_HOST_HPP
+
+#include "stat.hpp"
+#include <unordered_map>
+#include <vector>
+#include <string>
+
+class MostActiveHost : public Stat
+{
+public:
+  // N - the number of most active host
+  explicit MostActiveHost(size_t N);
+  void processLine(const LogEntry &) override;
+  std::string getStat() noexcept override;
+
+private:
+  struct Info {
+    size_t count;
+    bool isFirstN;
+    size_t idx;
+    Info(): count(0), isFirstN(false), idx(0) {}
+  };
+  const size_t N_;
+  // map from host to info
+  std::unordered_map<std::string, Info> infoMap_;
+  // sorted vector of N most active host
+  std::vector<std::string> firstN_;
+};
+
+#endif
