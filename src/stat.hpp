17c99564cfb132482b1fe0cd51f6d32adc89f688
Wenjun Ouyang <ouyangw@sas.upenn.edu>
2017-04-03 01:55:25 -0400

    Add base class for statistics stat.hpp.

---
+++ src/stat.hpp
@@ -0,0 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////
+// The base class for statistics.
+// Interfaces:
+//   processLine - process a line of input
+//   getStat - return a string as the stat
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef STAT_HPP
+#define STAT_HPP
+
+#include "log_entry.hpp"
+#include <string>
+
+class Stat
+{
+public:
+  virtual void processLine(const LogEntry &) = 0;
+  virtual std::string getStat() noexcept = 0;
+};
+
+#endif
