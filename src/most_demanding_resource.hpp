c6a2bc201f4d7a5b0c7a84f4c5746939e784929d
Wenjun Ouyang <ouyangw@sas.upenn.edu>
2017-04-04 00:35:31 -0400

    Add MostDemandingResource for statistics of bandwidth.

---
+++ src/most_demanding_resource.hpp
@@ -0,0 +1,38 @@
+////////////////////////////////////////////////////////////////////////////////
+// Maintain the information of N most active host
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef MOST_DEMANDING_RESOURCE_HPP
+#define MOST_DEMANDING_RESOURCE_HPP
+
+#include "stat.hpp"
+#include <unordered_map>
+#include <vector>
+#include <string>
+
+class MostDemandingResource : public Stat
+{
+public:
+  // N - the number of most demanding resource
+  explicit MostDemandingResource(size_t N);
+  void processLine(const LogEntry &) override;
+  std::string getStat() noexcept override;
+
+private:
+  void bubbleUp(size_t index) noexcept;
+
+private:
+  struct Info {
+    size_t accruedBytes;
+    bool isFirstN;
+    size_t idx;
+    Info(): accruedBytes(0), isFirstN(false), idx(0) {}
+  };
+  const size_t N_;
+  // map from resource to info
+  std::unordered_map<std::string, Info> infoMap_;
+  // sorted vector of N most demanding resource
+  std::vector<std::string> firstN_;
+};
+
+#endif
