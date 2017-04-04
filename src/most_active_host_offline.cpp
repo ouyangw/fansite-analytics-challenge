a733de1a19cfe72e7997007d968bed866f1d658b
Wenjun Ouyang <ouyangw@sas.upenn.edu>
2017-04-04 19:00:49 -0400

    Refactor offline approach to class MostActiveHostOffline and add main_feature1.cpp.

---
+++ src/most_active_host_offline.cpp
@@ -0,0 +1,41 @@
+#include "most_active_host_offline.hpp"
+#include <sstream>
+#include <algorithm>
+#include <vector>
+#include <functional>
+
+namespace
+{
+  struct HostHit {
+    size_t hits;
+    std::string host;
+    HostHit(size_t n, std::string h)
+        : hits(n)
+        , host(h)
+    {
+    }
+  };
+
+  bool operator>(const HostHit &lhs, const HostHit &rhs)
+  {
+    return lhs.hits == rhs.hits ? lhs.host < rhs.host : lhs.hits > rhs.hits;
+  }
+} // anonymous namespace
+
+////////////////////////////////////////////////////////////////////////////////
+
+std::string MostActiveHostOffline::getStat() noexcept
+{
+  // dump data in map to vector and partially sort the vector
+  std::stringstream stat;
+  std::vector<HostHit> vec;
+  vec.reserve(countMap_.size());
+  for (const auto &p: countMap_)
+    vec.emplace_back(p.second, p.first);
+  const size_t size(vec.size() > N_ ? N_ : vec.size());
+  std::partial_sort(vec.begin(), vec.begin() + size, vec.end(),
+                    std::greater<HostHit>());
+  for (size_t i(0); i < size; ++i)
+    stat << vec[i].host << ',' << vec[i].hits << '\n';
+  return stat.str();
+}
---
+++ src/most_active_host_offline.hpp
@@ -0,0 +1,45 @@
+////////////////////////////////////////////////////////////////////////////////
+// Feature 1: N most active host.
+// Offline calculation:
+//   1. Use hashmap to gather counts for each host
+//   2. Dump the hashmap into a vector and partially sort the vector
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef MOST_ACTIVE_HOST_OFFLINE_HPP
+#define MOST_ACTIVE_HOST_OFFLINE_HPP
+
+#include "stat.hpp"
+#include <unordered_map>
+
+class MostActiveHostOffline : public Stat
+{
+public:
+  // N - the number of most active host
+  explicit MostActiveHostOffline(size_t N);
+  void processLine(const LogEntry &) override;
+  std::string getStat() noexcept override;
+
+private:
+  const size_t N_;
+  // map from host to connection count
+  std::unordered_map<std::string, size_t> countMap_;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// inline implementations
+////////////////////////////////////////////////////////////////////////////////
+
+inline MostActiveHostOffline::MostActiveHostOffline(size_t N)
+    : N_(N)
+    , countMap_()
+{
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+inline void MostActiveHostOffline::processLine(const LogEntry &line)
+{
+  countMap_[line.host] += 1;
+}
+
+#endif
